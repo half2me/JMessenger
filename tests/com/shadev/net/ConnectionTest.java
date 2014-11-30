709191d8e1b7cdfcd5a761c0379468a98063feb8
Tamási Benjamin <halftome@yahoo.com>
2014-12-01 00:03:45 +0100

    Started testing...

---
+++ tests/com/shadev/net/ConnectionTest.java
@@ -0,0 +1,28 @@
+package com.shadev.net;
+
+import com.shadev.chat.ChatEventHandler;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.rmi.server.ExportException;
+
+import static org.junit.Assert.*;
+
+public class ConnectionTest {
+
+    private Connection c;
+    private ChatEventHandler ceh;
+
+    @Before
+    public void setUp() throws Exception {
+        ceh = new ChatEventHandler();
+        c = new Connection(ceh);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testIllegalConnect() throws Exception {
+        c.connect("localhost", 99999999);
+    }
+}
